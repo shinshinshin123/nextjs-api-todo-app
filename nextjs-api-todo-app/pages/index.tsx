af6ef0e66b18ec5217b647fe64a854b9c281f714
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-02-08 20:33:55 +0900

    todoのtopページ

--- nextjs-api-todo-app/pages/index.tsx
+++ nextjs-api-todo-app/pages/index.tsx
@@ -5,9 +5,10 @@ import styles from '@/styles/Home.module.css'
 
 // const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Todotop() {
   return (
-    <>
-    </>
+    <div>
+      <p>todoのトップページ</p>
+    </div>
   )
 }
